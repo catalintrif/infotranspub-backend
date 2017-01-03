50c0dbd757052d89b695233b1bfbc6f4ec2604ce
Catalin Trif <catalin.trif@gmail.com>
2017-01-03 21:27:54 +0100

    DB writer plugin, configuration file, DB script

--- tools/nutch-plugin/README.md
+++ tools/nutch-plugin/README.md
@@ -1,3 +1,18 @@
 # nutch-plugin
 Plugin pentru crawler-ul Apache Nutch (http://nutch.apache.org) care scrie intr-o tabela URL-urile nou gasite sau modificate,
 pornind de la lista de seed-uri.
+
+Instalare
+* Download distributie binara Nutch 1.12 http://www.apache.org/dyn/closer.lua/nutch/1.12/apache-nutch-1.12-bin.tar.gz
+* Dezarhivare (de preferat in directorul home):
+tar -xvf apache-nutch-1.12-bin.tar.gz
+* Setare JAVA_HOME (exemplu OSX)
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_77.jdk/Contents/Home
+* Verificare / modificare locatie instalare Nutch (proprietatea nutch.home) in pom.xml pentru a copia corect resursele dupa build
+* Build proiect (include copiere configurari Nutch si plugin-ul)
+mvn install
+* Copiere driver JDBC in directorul plugin-ului
+cd ~/apache-nutch-1.12
+cp ~/.m2/repository/org/postgresql/postgresql/9.4.1212/postgresql-9.4.1212.jar plugins/indexer-db/postgresql.jar
+* Lansare crawler
+./run.sh
--- tools/nutch-plugin/src/main/java/ro/gov/ithub/infotranspub/nutch/DBIndexer.java
+++ tools/nutch-plugin/src/main/java/ro/gov/ithub/infotranspub/nutch/DBIndexer.java
@@ -5,19 +5,31 @@ import org.apache.hadoop.mapred.JobConf;
 import org.apache.nutch.indexer.IndexWriter;
 import org.apache.nutch.indexer.IndexerMapReduce;
 import org.apache.nutch.indexer.NutchDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
 
 /**
- * Created by trifcat on 23.12.2016.
+ * Plugin pentru crawler-ul Apache Nutch (http://nutch.apache.org) care scrie intr-o tabela
+ * URL-urile nou gasite sau modificate.
+ * @author Catalin Trif
  */
 public class DBIndexer implements IndexWriter {
+    private static Logger LOG = LoggerFactory.getLogger(DBIndexer.class);
+    private static String SQL_SELECT = "SELECT hash FROM @table WHERE url = ?";
+    private static String SQL_INSERT = "INSERT INTO @table(url, hash, processed, modified_date) VALUES (?, ?, ?, ?)";
+    private static String SQL_UPDATE = "UPDATE @table SET hash = ?, processed = ?, modified_date = ? WHERE url = ?";
     private Configuration config;
-    private Writer writer;
+    private Connection connection;
     private boolean delete = false;
+    private PreparedStatement select;
+    private PreparedStatement insert;
+    private PreparedStatement update;
+
 
     public void open(JobConf job, String name) throws IOException {
         delete = job.getBoolean(IndexerMapReduce.INDEXER_DELETE, false);
@@ -25,50 +37,107 @@ public class DBIndexer implements IndexWriter {
 
     public void delete(String key) throws IOException {
         if (delete) {
-            writer.write("delete\t" + key + "\n");
+            String message = "Should delete: " + key;
+            LOG.info(message);
+            System.out.println(message);
         }
     }
 
     public void update(NutchDocument doc) throws IOException {
-        writer.write("update " + doc.getFieldValue("digest") + " " + doc.getFieldValue("id") + "\n");
+        write(doc);
     }
 
     public void write(NutchDocument doc) throws IOException {
-        writer.write("add " + doc.getFieldValue("digest") + " " + doc.getFieldValue("id") + "\n");
+        String url = doc.getFieldValue("id").toString();
+        String hash = doc.getFieldValue("digest").toString();
+        try {
+            select.setString(1, url);
+            ResultSet rs = select.executeQuery();
+            if (rs.next()) {
+                LOG.info("Found in DB: " + url);
+                String dbHash = rs.getString(1);
+                if (!hash.equals(dbHash)) {
+                    databaseUpdate(url, hash);
+                }
+            } else {
+                databaseInsert(url, hash);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            throw new IOException(e.getMessage());
+        }
+    }
+
+    private void databaseUpdate(String url, String hash) throws SQLException {
+        String message = "Updating in DB: " + url;
+        System.out.println(message);
+        LOG.info(message);
+        update.setString(1, hash);
+        update.setBoolean(2, false);
+        update.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+        update.setString(4, url);
+        update.executeUpdate();
+    }
+
+    private void databaseInsert(String url, String hash) throws SQLException {
+        String message = "Inserting in DB: " + url;
+        System.out.println(message);
+        LOG.info(message);
+        insert.setString(1, url);
+        insert.setString(2, hash);
+        insert.setBoolean(3, false);
+        insert.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
+        insert.executeUpdate();
     }
 
     public void close() throws IOException {
-        writer.flush();
-        writer.close();
+        try {
+            select.close();
+            update.close();
+            insert.close();
+            connection.close();
+        } catch (SQLException e) {
+            throw new IOException(e.getMessage());
+        }
     }
 
     public void commit() throws IOException {
-        writer.write("commit\n");
+        LOG.debug("commit()");
     }
 
-     public Configuration getConf() {
+    public Configuration getConf() {
         return config;
     }
 
     public void setConf(Configuration conf) {
         config = conf;
-        String path = conf.get("dummy.path");
-        if (path == null) {
-            String message = "Missing path. Should be set via -Ddummy.path";
-            message += "\n" + describe();
-            throw new RuntimeException(message);
+        try {
+            Class.forName("org.postgresql.Driver");
+            InputStream is = getConf().getConfResourceAsInputStream("indexer-db.properties");
+            Properties properties = new Properties();
+            properties.load(is);
+            String url = properties.getProperty("jdbc.url");
+            String user = properties.getProperty("user");
+            String password = properties.getProperty("password");
+            String table = properties.getProperty("table");
+
+            connection = DriverManager.getConnection(url, user, password);
+            LOG.info("Connected to: " + url);
+            select = connection.prepareStatement(SQL_SELECT.replaceFirst("@table", table));
+            insert = connection.prepareStatement(SQL_INSERT.replaceFirst("@table", table));
+            update = connection.prepareStatement(SQL_UPDATE.replaceFirst("@table", table));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
-        try {
-            writer = new BufferedWriter(new FileWriter(conf.get("dummy.path")));
-        } catch (IOException e) {
-        }
     }
 
     public String describe() {
-        StringBuffer sb = new StringBuffer("DBIndexer\n");
-        sb.append("\t").append(
-                "dummy.path : Path of the file to write to (mandatory)\n");
-        return sb.toString();
+        return "DBIndexer\n\tConfiguration file: $NUTCH_HOME/conf/indexer-db.properties\n";
     }
+
 }

da0b72223b3707d980f8e1c94d971a91bc2c01da
trifcat <catalin.trif@gmail.com>
2017-01-13 17:36:18 +0100

    UI improvements

--- sources/server/front-end/admin/agency.js
+++ sources/server/front-end/admin/agency.js
@@ -1,6 +1,7 @@
+// maps line IDs to arrays of stations
 var lines = new Map();
+// maps markers to stations for easy finding
 var markers = new Map();
-var currentMarker;
 var routePolyline;
 
 // elements
@@ -9,6 +10,15 @@ lineIdLabel = document.getElementById('lineIdLabel');
 stationNameInput = document.getElementById('stationName');
 stationLatInput = document.getElementById('stationLat');
 stationLngInput = document.getElementById('stationLng');
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  refreshScreen(lineIdInput.value, e.target.id);
+})
+$('#lineInfoDialog').on('shown.bs.modal', function() {
+  $('#stationName').focus();
+})
+
+// Event handlers
 
 function loadMap() {
     var mapCanvas = document.getElementById("map");
@@ -25,76 +35,147 @@ function loadMap() {
     routePolyline.setMap(map);
 }
 
-function onLineChange() {
-    drawRoute();
+function onLineChange(selected) {
+    refreshScreen(selected.value);
 }
 
 function onMapClick(map, position) {
-    currentMarker = new google.maps.Marker({
+    marker = new google.maps.Marker({
         position: position,
         draggable: true,
         map: map,
     });
-    newStationName = "S" + (markers.size + 1);
-    showStationDialog(position, newStationName);
-}
-
-function onMarkerClick(map, marker) {
-    var station = markers.get(marker);
-    showStationDialog(marker.getPosition(), station.name);
-    currentMarker = marker;
-}
-
-function showStationDialog(position, stationName) {
-    $('#lineInfoDialog').modal('show');
-    $('#lineInfoDialog').on('hidden.bs.modal', function (e) {
-        cancelDialog();
-    });
-    lineIdLabel.innerHTML = lineIdInput.value;
-    stationNameInput.value = stationName;
-    stationLatInput.value = position.lat();
-    stationLngInput.value = position.lng();
-}
-
-function saveLineButtonClicked() {
     lineId = lineIdInput.value;
+    showStationDialog(lineId, null, marker);
+}
+
+function onMarkerClick(map, marker, lineId) {
+    var station = markers.get(marker);
+    showStationDialog(lineId, station.id, marker);
+//    currentMarker = marker;
+}
+
+function showStationDialog(lineId, stationId, marker) {
+    $('#lineInfoDialog').modal('show');
+    $('#stationForm').unbind('submit').bind('submit', function(event, lineId){
+        // prevent default browser behaviour
+        event.preventDefault();
+        saveLineButtonClicked(lineId, stationId, marker);
+    });
+    $('#lineInfoDialog').on('hidden.bs.modal', function () {
+        cancelDialog(marker);
+    });
+
+    lineIdLabel.innerHTML = lineIdInput.value;
+    if (!marker) {
+        station = getStationById(stationName);
+        marker = station.marker;
+    }
+    stationLatInput.value = marker.getPosition().lat();
+    stationLngInput.value = marker.getPosition().lng();
+}
+
+function saveLineButtonClicked(lineId, stationId, marker) {
+    // TODO check why this is passed undefined
+    if (!lineId) {
+        lineId = lineIdInput.value;
+    }
     stationName = stationNameInput.value;
-    if (!markers.get(currentMarker)) { // this is a new station
-        currentMarker.setTitle(stationName);
+    if (!stationId) { // this is a new station
+        marker.setTitle(stationName);
         // TODO check why position is set to undefined
 //        position = new google.maps.LatLng(Number(stationLatInput.value), Number(stationLngInput.value));
-//        currentMarker.setPosition();
-        var marker = currentMarker;
+//        marker.setPosition();
         google.maps.event.addListener(marker, 'click', function() {
-            onMarkerClick(map, marker);
+            onMarkerClick(map, marker, lineId);
         });
-        google.maps.event.addListener(currentMarker, 'dragend', function() {
-            drawRoute();
+        google.maps.event.addListener(marker, 'dragend', function() {
+            drawRoute(lineId);
         });
+        stationId = "S" + markers.size;
+
+    // TODO only for development
+    if (!stationName) {
+        stationName = stationId;
+    }
+
         var station = {
+            id: stationId,
             name: stationName,
-            marker: currentMarker,
+            marker: marker,
             lines: [lineId]
         };
         setStationDescription(station);
-        markers.set(currentMarker, station);
+        markers.set(marker, station);
+        if (lines.get(lineId)) { // add station to line
+            lines.get(lineId).push(station);
+        } else { // first station on this line
+            lines.set(lineId, [station]);
+        }
     } else { // modify existing station
-        station = markers.get(currentMarker);
+        station = markers.get(marker);
         station.name = stationName;
         if (!station.lines.includes(lineId)) {
             station.lines.push(lineId);
         }
         setStationDescription(station);
     }
-    if (lines.get(lineId)) { // add station to line
-        lines.get(lineId).push(station);
-    } else { // first station on this line
-        lines.set(lineId, [station]);
-    }
     // close
-    showInfoWindow(map, currentMarker);
-    drawRoute();
+    $('#lineInfoDialog').modal('hide');
+    showInfoWindow(map, marker);
+    refreshScreen(lineId);
 }
+
+function cancelDialog(marker) {
+    if (!markers.get(marker)) {
+        marker.setMap(null);
+    }
+}
+
+// UI related
+
+function refreshScreen(lineId) {
+    drawRoute(lineId);
+    displayStations(lineId);
+}
+
+function displayStations(lineId) {
+    stationListDiv = document.getElementById('stationList');
+    var optionsHtml = '';
+    for (var station of lines.get(lineId)) {
+        optionsHtml +=
+            '<button type="button" class="list-group-item"' +
+            '   onclick="showStationDialog(' +
+                + lineId + ', \''+ station.name +'\', '+ null +')">' +
+            '    <span class="label label-warning">&nbsp;</span> ' + station.name +
+            '</button>';
+    }
+    stationListDiv = document.getElementById('stationList');
+    stationListDiv.innerHTML = optionsHtml;
+}
+
+function drawRoute(lineId) {
+    var path = [];
+    for (var station of lines.get(lineId)) {
+        path.push(station.marker.getPosition());
+    }
+    routePolyline.setOptions({
+        path: path,
+        strokeColor: "#0000FF",
+        strokeOpacity: 0.5,
+        strokeWeight: 4,
+        editable: true
+    });
+}
+
+function showInfoWindow(map, marker) {
+    var info = new google.maps.InfoWindow({
+        content: markers.get(marker).description
+    });
+    info.open(map, marker);
+}
+
+// Data related
 
 function setStationDescription(station) {
     var lineString = '';
@@ -104,32 +185,10 @@ function setStationDescription(station) {
     station.description = 'Stația ' + station.name + '<br>Linia ' + lineString;
 }
 
-function cancelDialog() {
-    if (!markers.get(currentMarker)) {
-        currentMarker.setMap(null);
-    }
-}
-
-function drawRoute() {
-    lineId = lineIdInput.value;
-    if (lines.get(lineId)) {
-        var path = [];
-        for (var station of lines.get(lineId)) {
-            path.push(station.marker.getPosition());
+function getStationById(id) {
+    for (var station of markers.values()) {
+        if (station.name == id) {
+            return station;
         }
-        routePolyline.setOptions({
-            path: path,
-            strokeColor: "#0000FF",
-            strokeOpacity: 0.5,
-            strokeWeight: 4,
-            editable: true
-        });
     }
-}
-
-function showInfoWindow(map, marker) {
-    var info = new google.maps.InfoWindow({
-        content: markers.get(marker).description
-    });
-    info.open(map, marker);
 }
